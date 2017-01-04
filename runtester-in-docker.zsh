3b6327105080fe4dfffec7a3c825f215c1afe6e1
KOBAYASI, Hiroaki <buribullet@gmail.com>
2017-01-04 14:03:57 +0900

    Add wrapper to run tester.t in docker.

---
+++ runtester-in-docker.zsh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+set -e
+
+emulate -L zsh
+
+scriptName=$(readlink -f $0)
+
+function usage {
+    cat 1>&2 <<EOF
+Usage: ${scriptName:t} TARGET_PROGRAM
+EOF
+    
+    exit 1
+}
+
+#========================================
+
+zparseopts -D -K i:=o_image
+
+if (($#o_image)); then
+    imageName=${${o_image[2]}#=}
+else
+    imageName=hkoba501/xhf-acceptance-inspection
+fi
+
+((ARGC)) || usage
+
+target=$(readlink -f $1); shift
+
+opts=(
+    -v ${target:h}:${target:h}
+    -v $PWD:$PWD
+    -w $PWD
+)
+
+docker run -it $imageName $opts $target
